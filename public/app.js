f03ac3a9cb24f9e68d4b51b9d6f15cd4969067f4
Abe Pasion <pasionabe@gmail.com>
2025-08-24 18:22:25 -0400

    Test

---
+++ public/app.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/compat/app";
+
+document.addEventListener('DOMContentLoaded', event => {
+    
+    const app = firebase.app();
+    
+});
+
+function googleLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+        .then((result) => {
+            const user = result.user;
+            console.log(`Hello ${user.displayName}`);
+        })
+        .catch((error) => {
+            console.error("Error during login", error);
+        });
+}
